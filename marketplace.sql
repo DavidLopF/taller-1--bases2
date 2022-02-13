c7fc606ee9460e873469f1a2116969659450a0d3
Your Name <davidyach26@gmail.com>
2022-02-13 18:37:51 -0500

    model products

--- marketplace.sql
+++ marketplace.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA users
 
     CREATE TABLE users.user(
-        
+
         id SERIAL PRIMARY KEY,
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
@@ -33,7 +33,41 @@ CREATE SCHEMA users
         id SERIAL PRIMARY KEY,
         user_id INTEGER NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users.user(id) ON DELETE CASCADE
-    );
+    )
+
+/*
+    Esquema productos en donde se guardan los productos normales, el tipo y los detalles del mismo
+*/
+
+CREATE SCHEMA products
+
+    CREATE TABLE products.product(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        quantity INTEGER NOT NULL,
+        trademark VARCHAR(255) NOT NULL,
+        price INTEGER NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+
+    CREATE TABLE products.product_type(
+        id SERIAL PRIMARY KEY,
+        product_id INTEGER NOT NULL,
+        type VARCHAR(255) NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES products.product(id) ON DELETE CASCADE
+    )
+    
+    CREATE TABLE products.product_clothe(
+        id SERIAL PRIMARY KEY,
+        product_id INTEGER NOT NULL,
+        size VARCHAR(255) NOT NULL,
+        color VARCHAR(255) NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES products.product(id) ON DELETE CASCADE
+    )
 
 
 
